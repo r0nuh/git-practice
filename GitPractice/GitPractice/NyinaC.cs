201f1e4a5ff8f688533a5cc2bc249f8bfdd64276
Nyina <dobinyina@gmail.com>
2018-02-16 11:35:06 +0100

    feat: c1

---
+++ GitPractice/GitPractice/NyinaC.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitPractice
+{
+    class NyinaC
+    {
+        public void Cheer()
+        {
+            Console.WriteLine("Hi Chrysocolla Team!");
+        }
+    }
+}
