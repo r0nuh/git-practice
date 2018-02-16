6c2f0506435b768350162b80fa6ad9ce7038391a
r0nuh <cs.szilard@gmail.com>
2018-02-16 11:36:23 +0100

    C1

---
+++ GitPractice/GitPractice/SzilardCheer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitPractice
+{
+    class SzilardCheer
+    {
+        public static void Cheer()
+        {
+            Console.WriteLine("Chrysocolla!!!!");
+        }
+    }
+}
