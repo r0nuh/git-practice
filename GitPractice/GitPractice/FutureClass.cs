068cf88dd30155bda6ee971310765ee86cf6a1df
csullogesztee <csullogesztee@gmail.com>
2018-02-16 11:34:24 +0100

    d1

---
+++ GitPractice/GitPractice/FutureClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitPractice
+{
+    class FutureClass
+    {
+        public void Future()
+        {
+            Console.WriteLine("Capricorn");
+        }
+    }
+}
