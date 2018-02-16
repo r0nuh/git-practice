e545e86f61862c716f2e8df76c52516499d734c7
Nyina <dobinyina@gmail.com>
2018-02-16 11:57:31 +0100

    feat: c5.1

--- GitPractice/GitPractice/NyinaC.cs
+++ GitPractice/GitPractice/NyinaC.cs
@@ -6,10 +6,10 @@ namespace GitPractice
 {
     class NyinaC
     {
-        public void Cheer(string teamName)
+        public void Cheer(string greet, string teamName)
         {
             teamName = "Chrysocolla";
-            Console.WriteLine("Hi {0} Team!", teamName);
+            Console.WriteLine("Hi {0} {1} Team!", greet, teamName);
         }
     }
 }
