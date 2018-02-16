8d7dcf5eaa3b59b4e89f956cd5e5bb146f60f803
PataiSzabolcs <pataiszabolcs@gmail.com>
2018-02-16 11:34:13 +0100

    b1

---
+++ GitPractice/GitPractice/AgePrinter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitPractice
+{
+    class AgePrinter
+    {
+        public void PrintAge()
+        {
+            Console.WriteLine(27);
+        }
+    }
+}
